5d68f375951fa70a3deb52e9ed50eb0cb2f3777b
bzaffhass <hassanzaff@gmail.com>
2024-04-03 15:46:28 +0100

    Addition of Bank class

---
+++ src/Bank.java
@@ -0,0 +1,56 @@
+//Hassan
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+// private instance variables
+public class Bank {
+    private idAccountMap HashMap<String, BankAccount>;
+    private idList ArrayList<String>;
+
+    //Constructor
+
+    public Bank() {
+        idAccountMap = new HashMap<>();
+        idList = new ArrayList<>();
+    }
+
+
+    public void addAccount(String id, BankAccount account) {
+        idAccountMap.put(id, account);
+        idList.add(id);
+    }
+
+    public boolean removeAccount(String id) {
+        if (idList.contains(id)) {
+            idAccountMap.remove(id);
+            idList.remove(id);
+            return true;
+        }
+        return false;
+    }
+
+    public void printAllAccounts() {
+        for (String id : idList) {
+            if (idAccountMap.containsKey(id)) {
+                BankAccount account = idAccountMap.get(id);
+                account.printDetails();
+            }
+        }
+    }
+
+    public void printAllSavingsAccounts() {
+        for (String id : idList)
+
+
+            public void printAllCurrentAccounts() {
+            for (String id : idList) {
+                if (idAccountMap.containsKey(id)) {
+                    BankAccount account = idAccountMap.get(id);
+                    if (account instanceof CurrentAccount) {
+                        account.printDetails();
+                    }
+                }
+            }
+        }
+    }
